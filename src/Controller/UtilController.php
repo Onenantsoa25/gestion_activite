b368616910726e3e1b6c04f8222bbef37b35b06a
Onenantsoa25 <152161654+Onenantsoa25@users.noreply.github.com>
2025-08-01 14:02:01 +0300

    test-twig
    Apprentissage de symfony, twig. Creation controller, service et repository pour test

--- src/Controller/UtilController.php
+++ src/Controller/UtilController.php
@@ -15,41 +15,24 @@ class UtilController extends AbstractController {
         $this->utilService = $utilService;
     }
 
-    #[Route('/utils', name: 'util_list', methods: 'GET')]
+    #[Route('/utils', name: 'util_list', methods: ['GET'])]
     public function list_util(): Response {
         $utils = $this->utilService->findAllUtil();
-
-        ob_start();
-        extract(['utils' => $utils]);
-
-        include __DIR__ . '/../../templates/util_list.php';
-
-        $contenu = ob_get_clean();
-        return new Response($contenu);
+        return $this->render('util_list.html.twig', ['utils' => $utils]);
     }
 
-    #[Route("/util", name: "insert_util", methods: 'POST')]
+    #[Route("/util", name: "insert_util", methods: ['POST'])]
     public function insert_util(Request $request): Response {
         $nom = $request->request->get("nom");
         $util = new Util();
         $util->setName($nom);
         $this->utilService->insert($util);
 
-        ob_start();
-        include __DIR__ . '/../../templates/reussi.php';
-        $contenu = ob_get_clean();
-
-        return new Response($contenu);
+        return $this->render('reussi.html.twig');
     }
 
-    #[Route("/util", name: "insert_util_page", methods: 'GET')]
+    #[Route("/util", name: "insert_util_page", methods: ['GET'])]
     public function insert_util_page(): Response {
-
-        ob_start();
-        include __DIR__ . '/../../templates/insert_util.php';
-        $contenu = ob_get_clean();
-
-        return new Response($contenu);
+        return $this->render('insert_util.html.twig');
     }
-
 }
